a3b8e7c352b10d90c8c3d454a63d171c13337c1e
tejalpadharia <36251006+tejalpadharia@users.noreply.github.com>
2018-03-09 15:06:42 -0800

    Validation
    added validations to the feilds

--- src/components/Welcome.js
+++ src/components/Welcome.js
@@ -2,6 +2,10 @@ import React, {Component} from 'react';
 import {Link,withRouter} from 'react-router-dom';
 import PropTypes from 'prop-types';
 import * as API from '../api/API';
+import validator from 'validator';
+import className from 'classnames';
+let styleError = {color:'red'}
+
 class Welcome extends Component {
 
     static propTypes = {
@@ -16,14 +20,18 @@ class Welcome extends Component {
             no1: '',
             no2: '',
             result: '',
-            message: ''
+            message: '',
+            num1Error:'',
+            num2Error: ''
         };
 
     componentWillMount(){
         this.setState({
             username : this.props.username,
             no1: '',
-            no2: ''
+            no2: '',
+            num1Error:'',
+            num2Error: ''
         });
         //document.title = `Welcome, ${this.state.username} !!`;
     }
@@ -32,55 +40,109 @@ class Welcome extends Component {
         document.title = `Welcome, ${this.state.username} !!`;
     }
 
-    handleAdd = () => {
-        // if(userData.no1 == undefined || userData.no1 == undefined ){
-        //     this.setState({ no1:document.getElementById('Number1').value ,
-        //         no1:document.getElementById('Number2').value});
-        // } else {
-        //     this.setState({
-        //         no1: userData.no1,
-        //         no2: userData.no2
-        //     });
-        // }
-        console.log(this.state);
-        API.doAdd(this.state)
-            .then((res) => {
+    handleClear = () =>{
+        document.getElementById('Number1').value = '';
+        document.getElementById('Number2').value = '';
+        document.getElementById('ErrorNum1').innerHTML = '';
+        document.getElementById('ErrorNum2').innerHTML = '';
+        document.getElementById('answer').innerHTML = '';
+        this.state.no1 = '';
+        this.state.no2='';
+        this.state.message = '';
+    }
 
-                this.setState({
-                    message: res.message
+    validate = () => {
+        let isError = false;
+        const errors = {};
+        if(this.state.no1.length < 1 ){
+            isError = true;
+            this.state.num1Error = 'Kindly enter number';
+        }
+        else if(isNaN(this.state.no1)){
+            isError = true;
+            this.state.num1Error = "Enter a valid number";
+        }
+        else{
+            this.state.num1Error='';
+        }
+
+        if(this.state.no2.length < 1 ){
+            isError = true;
+            errors.num2Error = 'Kindly enter number';
+        }
+        else if(isNaN(this.state.no2)){
+            isError = true;
+            this.state.num1Error = "Enter a valid number";
+        }
+        else{
+            this.state.num2Error='';
+        }
+        if(isError){
+            this.setState({
+                ...this.state,
+                ...errors
+            });
+        }
+        return isError;
+    }
+
+    handleAdd = () => {
+        const err = this.validate();
+        if(!err) {
+            API.doAdd(this.state)
+                .then((res) => {
+                    this.setState({
+                        message: res.message
+                    });
                 });
-            } );
+        }
     };
 
     handleSub = () => {
-        API.doSub(this.state)
-            .then((res) => {
-
-                this.setState({
-                    message: res.message
-                });
-            } );
-    };
-
+        const err = this.validate();
+        if(!err){
+            API.doSub(this.state)
+                .then((res) => {
+                    this.setState({
+                        message: res.message
+                    });
+                } );
+            }
+        };
 
     handleMultiply = () => {
-        API.doMultiply(this.state)
-            .then((res) => {
+        const err = this.validate();
+        if(!err) {
+            API.doMultiply(this.state)
+                .then((res) => {
 
-                this.setState({
-                    message: res.message
+                    this.setState({
+                        message: res.message
+                    });
                 });
-            } );
+        }
     };
 
     handleDivide = () => {
-        API.doDivide(this.state)
-            .then((res) => {
+        const err = this.validate();
+        if(!err) {
+            if(this.state.no2 == 0 ){
+                this.state.num2Error = "Divide by zero is undefined";
+                document.getElementById('ErrorNum2').innerHTML = this.state.num2Error;
+                document.getElementById('Number1').value = '';
+                document.getElementById('Number2').value = '';
+                this.state.no1 = '';
+                this.state.no2='';
+            } else{
+            API.doDivide(this.state)
+                .then((res) => {
 
-                this.setState({
-                    message: res.message
+                    this.setState({
+                        message: res.message
+                    });
                 });
-            } );
+            }
+        }
     };
 
     render(){
@@ -100,12 +162,13 @@ class Welcome extends Component {
                                 <div className="form-group">
                                     <h3>Calculator</h3>
                                 </div>
-                                <div className="form-group">
+                                <div className="numberGroupClass">
                                     <input className="form-control" type="text" label="Number1" placeholder="Enter Number" id="Number1"
                                            value={this.state.no1}
                                            onChange={(event) => {this.setState ({no1: event.target.value});
                                            }}
                                     />
+                                    <p id='ErrorNum1' className="help-block" style={styleError}> {this.state.num1Error? this.state.num1Error: ''}</p>
                                 </div>
 
                                 <div className="form-group">
@@ -115,6 +178,7 @@ class Welcome extends Component {
                                         onChange={(event) => {this.setState ({ no2: event.target.value});
                                         }}
                                     />
+                                    <p id='ErrorNum2' className="help-block" style={styleError}> {this.state.num2Error? this.state.num2Error: ''}</p>
                                 </div>
                                 <div className="form-group">
                                     <button className="btn btn-primary" type="button" onClick={() => this.handleAdd()}>
@@ -129,7 +193,7 @@ class Welcome extends Component {
                                     <button className="btn btn-primary" type="button" onClick={() => this.handleDivide()}>
                                         /
                                     </button>&nbsp;
-                                    <button className="btn btn-primary" type="button" onClick={() => this.handleSubmit()}>
+                                    <button className="btn btn-primary" type="button" onClick={() => this.handleClear()}>
                                         Clear
                                     </button>&nbsp;
                                 </div>
@@ -140,7 +204,7 @@ class Welcome extends Component {
                         <div className="col-md-3">
                             {this.state.message && (
                                 <div className="alert alert-warning" role="alert">
-                                    {this.state.message}
+                                    <p id="answer">{this.state.message}</p>
                                 </div>
                             )}
                         </div>
